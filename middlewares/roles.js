8e2e7ebd0081545519f5b228dc167df12d1d2498
Rehmet yeshanew <rehmetyeshanew@gmail.com>
2020-10-28 22:11:30 +0300

    second commit

--- middlewares/roles.js
+++ middlewares/roles.js
@@ -5,8 +5,7 @@ exports.roles = async(req,res,next)=>{
         const broker = await brokerModel.findOne({
         roles: req.body.roles
     })
-    //.populate({ path: 'roles', populate: {path: 'permissions'} });
-    //broker._doc.roles = broker._doc.roles.map(role => role.name)
+  
          if(broker){
              broker.insertOne(
             { roles: ['broAct']}
