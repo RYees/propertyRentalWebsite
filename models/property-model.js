6526ced04ef16a55337c8374e7d1fe8b43cc297f
Rehmet yeshanew <rehmetyeshanew@gmail.com>
2020-10-29 23:39:30 +0300

    property,broker,admin,permissionandroles models

--- models/property-model.js
+++ models/property-model.js
@@ -2,13 +2,35 @@
 const mongoose = require('mongoose');
 const mongoosePaginate = require('mongoose-paginate-v2');
 const bcrypt = require('bcrypt')
-
+const reqString = {
+    type:String,
+    required:true
+  }
+  const priceSchema = new mongoose.Schema({
+    amount: reqString,
+    type: reqString
+  })
+  const hcontentSchema = new mongoose.Schema({
+    bedrooms: reqString,
+    bathrooms: reqString,
+    no_of_floors: reqString,
+    amenities: reqString,
+  })
+  const addressSchema = new mongoose.Schema({
+    sub_city:reqString,
+    city:reqString,
+    area:reqString,
+    coordinates:reqString
+})
 const propertySchema = new mongoose.Schema({
     //_id: mongoose.Schema.Types.ObjectId,
     prop_type: {type: String, default: ''},
     address: { type: String, default: '' },
-    price: {type : String, default: ''},
+    price: [priceSchema],
     image: { type: String, default:'' },
+    prop_contents:[hcontentSchema],
+    notes: { type: String, default:'' },
+    area_in_m2: { type: String, required:true },
     broker: { type: mongoose.Schema.Types.ObjectId, ref: 'Broker', required:false },
     active: { type: Boolean, default: true },
     push_token: { type: String, default: '' },
